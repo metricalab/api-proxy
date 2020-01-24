9e4ccd3857ba68e86e378ede80137cff51c8032a
fp-ultano <ultano@flyingpigs.es>
2020-01-24 10:00:27 +0100

    created Maven project

---
+++ src/main/java/com/metrica/api/proxy/ApiProxyApplication.java
@@ -0,0 +1,13 @@
+package com.metrica.api.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProxyApplication.class, args);
+	}
+
+}
